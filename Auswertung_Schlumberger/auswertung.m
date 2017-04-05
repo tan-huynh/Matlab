e41f81b7f00b0dd46b1bf7d2fd7c0a21e50a4dc3
Tan Huynh Ngoc <huynh.ngoctan@yahoo.com>
2017-04-05 11:15:21 +0200

    Create auswertung.m

---
+++ Auswertung_Schlumberger/auswertung.m
@@ -0,0 +1,22 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Auswertung von Schlumberger Sweep
+%% Autor: Huynh Ngoc Tan
+%% Email: tan.huynhngoc@mabel.info / huynh.ngoctan@yahoo.com
+%% MABEL Insitut, Hochschule Mannheim, Deutschland
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+addpath C:\Users\tan.huynhngoc\Desktop\TestPTT_Matlab\Schlumberg_Auswertung
+clear all, clc, close all;
+Arr_File = {
+    'Schlumberger_Messformular_piezo1.xls' ...
+    'Schlumberger_Messformular_piezo2.xls' ...
+    'Schlumberger_Messformular_piezo3.xls'
+};
+data = [Arr_File{3}];
+A = xlsread(data);
+Freq = A(:,1); Impedanz = A(:,2); Phase = A(:,3);
+figure, semilogy(Freq,Phase,'k.-',Freq,Impedanz,'r-'), grid on,
+xlabel('Frequenz / Hz'), ylabel('Dek / dB'),
+legend('Phase','Impedanz');
